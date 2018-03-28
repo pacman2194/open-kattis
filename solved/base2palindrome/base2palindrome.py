695f031d36f6a67ddbec32e0b98fbd3b8eca0fd2
pacman2194 <sad2emfp@gmail.com>
2018-03-28 12:00:24 -0400

    practice

---
+++ solved/base2palindrome/base2palindrome.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+ans = int(input()) - 1
+c = int(1)
+leng = int(0)
+while ans > 1:
+    if '0' in bin(c)[2::]:
+        c += 1
+    elif leng == 0:
+        leng = 1
+        c += 1
+    else:
+        leng = 0
+        c = int(c / 2) + 1
+    ans -= 1
+if ans == 0:
+    print(1)
+elif leng == 0:
+    print(int((bin(c)[2::] + bin(c)[:1:-1]), 2))
+else:
+    print(int((bin(c)[2::] + bin(c)[-2:1:-1]), 2))  # modify
---
+++ solved/flyingsafely/flyingsafely.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+cases = int(input())
+for case in range(cases):
+    c, p = [int(x) for x in input().split()]
+    for pilot in range(p):
+        input()
+    print(c - 1)
